f7673ae123f4b9434418c6ca80797150ea973029
Giulia Farina <giuliafarina88@gmail.com>
2021-12-03 11:48:31 +0100

    add temporary solution innerhTML

--- src/components/products/product-card.js
+++ src/components/products/product-card.js
@@ -2,40 +2,15 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import * as styles from "./product-card.module.css"
 
-
-
-
-
-const size = {
-  mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
-  desktop: '2560px'
-}
-
-
-export const device = {
-  mobileS: `(min-width: ${size.mobileS})`,
-  mobileM: `(min-width: ${size.mobileM})`,
-  mobileL: `(min-width: ${size.mobileL})`,
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`
-};
-
-
-
 const ProductCard = (props) => {
   const data = useStaticQuery(graphql`
 query Product {
    allContentfulResource(sort: {fields: createdAt, order: ASC}) {
     nodes {
       id
+       childContentfulResourceDescriptionTextNode {
+          description
+        }
       childContentfulResourceDescriptionTextNode {
         childrenMarkdownRemark{
            excerpt(format: HTML)
