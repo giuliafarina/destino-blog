2b500c4f6a179ae7870843b2dcdcabbbe1487d01
Giulia Farina <giuliafarina88@gmail.com>
2021-12-10 13:50:33 +0100

    add media query for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
       <div></div>
-      <p>Sign up for my <a href="https://mailchi.mp/3ea47faf06e0/h1rtvyb3sz">Newsletter </a></p>
+      <h3>Sign up for my <a href="https://mailchi.mp/3ea47faf06e0/h1rtvyb3sz">Newsletter </a></h3>
       <p className={styles.reference}>Designed and coded by <a href="https://giuliafarina.com/">Giulia Farina</a></p>
     </div>
   </Container>
