48f89afb5a940f7bf19654748e5d1586f5cf53d7
Giulia Farina <giuliafarina88@gmail.com>
2021-12-10 13:44:08 +0100

    install plugin algolia and add styled components

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,11 +6,9 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Designed and coded by <a href="https://giuliafarina.com/">Giulia Farina</a>
-      <p>Sign up for my Newsletter <a href="https://mailchi.mp/3ea47faf06e0/h1rtvyb3sz">here</a></p>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-
+      <div></div>
+      <p>Sign up for my <a href="https://mailchi.mp/3ea47faf06e0/h1rtvyb3sz">Newsletter </a></p>
+      <p className={styles.reference}>Designed and coded by <a href="https://giuliafarina.com/">Giulia Farina</a></p>
     </div>
   </Container>
 )
