368d9d780da80b404938703364ea662120a43c36
Giulia Farina <giuliafarina88@gmail.com>
2021-12-13 13:09:34 +0100

    add target blank for footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
       <div></div>
-      <h3>Sign up for my <a href="https://mailchi.mp/3ea47faf06e0/h1rtvyb3sz">Newsletter </a></h3>
+      <h3>Sign up for my <a href="https://mailchi.mp/3ea47faf06e0/h1rtvyb3sz" target="_blank">Newsletter </a></h3>
       <p className={styles.reference}>Designed and coded by <a href="https://giuliafarina.com/">Giulia Farina</a></p>
     </div>
   </Container>
