b0d8d6d2c62814a8edd5e216881959b26897231e
Giulia Farina <giuliafarina88@gmail.com>
2022-07-17 17:59:11 +0200

    remove discount h2

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,7 +10,6 @@ const Hero = ({ image, title, content, discount }) => (
     <div className={styles.details}>
       <h1 className={styles.title}>{title}</h1>
       {content && <p className={styles.content}>{content}</p>}
-      <h2 className={styles.discount}>{discount}</h2>
     </div>
   </div>
 )
